b3a1e1a11b3edb7e5683b84cf225a21f820edbda
Karl <karl.hopkinson-turrell@nearform.com>
2018-07-30 10:46:03 +0000

    Move route to a separate resource to avoid bug with changes every plan

--- root.tf
+++ root.tf
@@ -79,14 +79,15 @@ resource "aws_route_table" "public_routes" {
 resource "aws_route_table" "private_routes" {
     vpc_id = "${aws_vpc.aws_proxy_pattern_vpc.id}"
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        network_interface_id = "${aws_network_interface.proxy.id}"
-    }
-
     tags {
         Name = "private_routes"
     }
+}
+
+resource "aws_route" "proxy_route" {
+    route_table_id = "${aws_route_table.private_routes.id}"
+    destination_cidr_block = "0.0.0.0/0"
+    network_interface_id = "${aws_network_interface.proxy.id}"
 }
 
 resource "aws_route_table_association" "public_routes_association" {
